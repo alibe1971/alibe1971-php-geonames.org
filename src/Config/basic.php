b30712209145519297b18e921d004e766a2c9e88
Andrea Liberati <25078094+alibe1971@users.noreply.github.com>
2020-08-27 22:24:48 +0100

    First commit

---
+++ src/Config/basic.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |------------------------------------------------------
+    | Config basic parameters
+    |------------------------------------------------------
+    |
+    | Params for the connection to GeoNames
+    |
+    */
+
+    'version'=>'0.0.0',
+    'clID'=>'GeoNames.Org ID'
+
+];
---
+++ src/geonames.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace alibe\phpGeonamesorg;
+
+class geonames {
+
+
+}
